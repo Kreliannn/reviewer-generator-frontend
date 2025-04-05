9d4421715fdd6931c473923df113f63d863b6d33
Kreliannn <krelianquimson@gmail.com>
2025-04-05 22:04:57 +0800

    modify chart

--- src/app/pages/quizTaking/components/chart.tsx
+++ src/app/pages/quizTaking/components/chart.tsx
@@ -19,9 +19,9 @@ import {
 } from "@/components/ui/chart"
 
 const chartData = [
-    { answer: "correct", type: 15, fill: "#22c55e" }, 
-    { answer: "wrong", type: 20, fill: "#ef4444" },  
-    { answer: "pass", type: 5, fill: "blue" },    
+  { answer: "correct", type: 49, fill: "#22c55e" }, // green
+  { answer: "wrong", type: 21, fill: "#ef4444" },   // red
+  { answer: "pass", type: 1, fill: "blue" },        // blue
 ]
 
 const chartConfig = {
@@ -44,7 +44,7 @@ const chartConfig = {
 
 export default function Component() {
   return (
-    <Card className="flex flex-col">
+    <Card className="flex flex-col bg-stone-800 text-white shadow-sm">
       <CardHeader className="items-center pb-0">
         <CardTitle>Pie Chart - Answer Summary</CardTitle>
         <CardDescription>Quiz Results (Jan - June 2024)</CardDescription>
@@ -52,22 +52,34 @@ export default function Component() {
       <CardContent className="flex-1 pb-0">
         <ChartContainer
           config={chartConfig}
-          className="mx-auto aspect-square max-h-[250px] [&_.recharts-text]:fill-background"
+          className="mx-auto aspect-square max-h-[300px] [&_.recharts-text]:fill-background"
         >
           <PieChart>
             <ChartTooltip
               content={<ChartTooltipContent nameKey="answer" hideLabel />}
             />
-            <Pie data={chartData} dataKey="type">
-              <LabelList
-                dataKey="answer"
-                className="fill-background font-bold"
-                stroke="none"
-                fontSize={15}
-                formatter={(value: keyof typeof chartConfig) =>
-                  chartConfig[value]?.label
-                }
-              />
+            <Pie
+              data={chartData}
+              dataKey="type"
+              outerRadius={80}
+              label={({ name, value, index }) => chartData[index].answer}
+
+
+              labelLine={true}
+             
+            >
+                <LabelList
+                    dataKey="type"
+                    className="fill-background font-bold"
+                    stroke="none"
+                    fontSize={14}
+                    formatter={(value: number) => {
+                        const total = chartData.reduce((acc, cur) => acc + cur.type, 0)
+                        const percent = ((value / total) * 100).toFixed(1)
+                        return `${percent}%`
+                }}
+                />
+
             </Pie>
           </PieChart>
         </ChartContainer>
